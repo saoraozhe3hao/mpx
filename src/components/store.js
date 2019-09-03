04809aa31865befeb6b65e93f8f05db7a27cda05
unknown <saoraozhe3hao@126.com>
2019-09-03 19:53:15 +0800

    api

--- src/components/store.js
+++ src/components/store.js
@@ -2,10 +2,15 @@ import {createStore} from '@mpxjs/core'
 
 const store = createStore({
   state: {
+    location: 3,
     userInfo: {}
   },
   mutations: {
     change () {
+      console.log(33)
+    },
+    setLocation(state, payload) {
+      state.location = payload;
     }
   }
 });
