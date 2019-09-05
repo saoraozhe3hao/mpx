5043a77d631de8f0fc1afccd8d2515740d39897f
unknown <saoraozhe3hao@126.com>
2019-09-05 17:39:57 +0800

    地理位置

--- src/components/store.js
+++ src/components/store.js
@@ -3,7 +3,10 @@ import {createStore} from '@mpxjs/core'
 const store = createStore({
   state: {
     isLogin: false,
-    location: {},
+    systemInfo: {},
+    area: '全国', // 当前选择的 搜索区域，格式为 省-市-县
+    location: {},  // 当前定位的 经纬度
+    address: {},   // 当前定位 的 省市县
     userInfo: {}
   },
   mutations: {
