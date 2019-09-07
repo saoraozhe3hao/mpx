ccd4dd9fa11d2b901ab51c31c57335b98e40475e
unknown <saoraozhe3hao@126.com>
2019-09-07 18:30:45 +0800

    api

--- src/components/store.js
+++ src/components/store.js
@@ -7,7 +7,9 @@ const store = createStore({
     area: '全国', // 当前选择的 搜索区域，格式为 省-市-县
     location: {},  // 当前定位的 经纬度
     address: {},   // 当前定位 的 省市县
-    userInfo: {}
+    userInfo: {},  // 小程序本地获取到的用户信息
+    customerInfo: {}, // 从后端获取的用户信息
+    merchantInfo: {} // 从后端获取的趣导信息
   },
   mutations: {
     change () {
